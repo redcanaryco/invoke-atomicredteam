6a36b213db321e1e5d4969d28ac8812eefbbcf83
clr2of8 <clr2of8@gmail.com>
2020-03-07 13:06:43 -0700

    add session everywhere

--- Public/Invoke-AtomicTest.ps1
+++ Public/Invoke-AtomicTest.ps1
@@ -207,14 +207,14 @@ function Invoke-AtomicTest {
                             $final_command_prereq = Merge-InputArgs $dep.prereq_command $test $InputArgs $PathToAtomicsFolder
                             if($executor -ne "powershell") { $final_command_prereq = ($final_command_prereq.trim()).Replace("`n", " && ") }
                             $final_command_get_prereq = Merge-InputArgs $dep.get_prereq_command $test $InputArgs $PathToAtomicsFolder
-                            $res = Invoke-ExecuteCommand $final_command_prereq $executor $TimeoutSeconds
+                            $res = Invoke-ExecuteCommand $final_command_prereq $executor $TimeoutSeconds $session
 
                             if ($res -eq 0) {
                                 Write-KeyValue "Prereq already met: " $description
                             }
                             else {
-                                $res = Invoke-ExecuteCommand $final_command_get_prereq $executor $TimeoutSeconds 
-                                $res = Invoke-ExecuteCommand $final_command_prereq $executor $TimeoutSeconds
+                                $res = Invoke-ExecuteCommand $final_command_get_prereq $executor $TimeoutSeconds $session
+                                $res = Invoke-ExecuteCommand $final_command_prereq $executor $TimeoutSeconds $session
                                 if ($res -eq 0) {
                                     Write-KeyValue "Prereq successfully met: " $description
                                 }
@@ -230,7 +230,7 @@ function Invoke-AtomicTest {
                     elseif ($Cleanup) {
                         Write-KeyValue "Executing cleanup for test: " $testId
                         $final_command = Merge-InputArgs $test.executor.cleanup_command $test $InputArgs $PathToAtomicsFolder
-                        $res = Invoke-ExecuteCommand $final_command $test.executor.name $TimeoutSeconds
+                        $res = Invoke-ExecuteCommand $final_command $test.executor.name $TimeoutSeconds $session
                         Write-KeyValue "Done executing cleanup for test: " $testId
                     }
                     else {
