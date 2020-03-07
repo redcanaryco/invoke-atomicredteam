162af377d340ca5359e7c45159bf1ff01ac2db5a
clr2of8 <clr2of8@gmail.com>
2020-03-07 13:40:48 -0700

    todo notes

--- Private/Invoke-ExecuteCommand.ps1
+++ Private/Invoke-ExecuteCommand.ps1
@@ -24,6 +24,7 @@ function Invoke-ExecuteCommand ($finalCommand, $executor, $TimeoutSeconds, $sess
             $scriptParentPath = Split-Path $import -Parent
             $fp = Join-Path $scriptParentPath "Invoke-Process.ps1"
             $fp2 = Join-Path $scriptParentPath "Invoke-KillProcessTree.ps1"
+            #need maldoc file and verifyfilehash
             invoke-command -Session $session -FilePath $fp
             invoke-command -Session $session -FilePath $fp2
             invoke-command -Session $session -ScriptBlock { Invoke-Process -filename $Using:execExe -Arguments $Using:arguments -TimeoutSeconds $Using:TimeoutSeconds }
