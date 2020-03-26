09a33554bdafbae26eb85d219467e2f96ff904d3
clr2of8 <clr2of8@gmail.com>
2020-03-26 09:28:58 -0600

    little PSv5.0 fix

--- Invoke-AtomicRedTeam.psm1
+++ Invoke-AtomicRedTeam.psm1
@@ -2,7 +2,7 @@
 
 #Get public and private function definition files.
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
-$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -Exclude "AtomicClassSchema.ps1" -ErrorAction SilentlyContinue )
 
 #Dot source the files
 Foreach ($import in @($Public + $Private)) {
