2caf4025bd4124adfdd9c85a8aae2dff79af16f4
clr2of8 <clr2of8@gmail.com>
2020-12-08 09:17:41 -0700

    add option to pass file containing macro

--- Public/Invoke-MalDoc.ps1
+++ Public/Invoke-MalDoc.ps1
@@ -7,6 +7,8 @@ function Invoke-MalDoc {
     A module to programatically execute Microsoft Word and Exel Documents containing macros. The module will temporarily add a registry key to allow PowerShell to interact with VBA.
     .PARAMETER macroCode
     [Required] The VBA code to be executed. By default, this macro code will be wrapped in a sub routine, called "Test" by default. If you don't want your macro code to be wrapped in a subroutine use the `-noWrap` flag. To specify the subroutine name use the `-sub` parameter.
+    .PARAMETER macroFile
+    [Required] A file containing the VBA code to be executed. To specify the subroutine name to be called use the `-sub` parameter.
     .PARAMETER officeVersion
     [Optional] The Microsoft Office version to use for executing the document. e.g. "16.0". The version will be determined Programmatically if not specified.
     .PARAMETER officeProduct
@@ -36,10 +38,16 @@ function Invoke-MalDoc {
     C:\PS> Invoke-Maldoc -macroCode "Sub Exec()`nMsgBox `"Hello`"`nEnd Sub" -officeProduct "Word" -noWrap -sub "Exec"
     -----------
     Create a macro enabled Microsoft Word Document. The macroCode will be unmodified (i.e. not wrapped insided a subroutine) and the "Exec" subroutine will be executed.
+
+    .EXAMPLE
+    C:\PS> Invoke-Maldoc -macroFile "C:\AtomicRedTeam\atomics\T1003\src\macro.txt" -officeProduct "Word" -sub "DoIt"
+    -----------
+    Create a macro enabled Microsoft Word Document. The macroCode will be read from the specified file and the "DoIt" subroutine will be executed.
+
 #>
 
     Param(
-        [Parameter(Position = 0, Mandatory = $True)]
+        [Parameter(Position = 0, Mandatory = $True, ParameterSetName = "code")]
         [String]$macroCode,
 
         [Parameter(Position = 1, Mandatory = $False)]
@@ -52,18 +60,26 @@ function Invoke-MalDoc {
         [Parameter(Position = 3, Mandatory = $false)]
         [String]$sub = "Test",
 
-        [Parameter(Position = 4, Mandatory = $false)]
-        [switch]$noWrap
+        [Parameter(Position = 4, Mandatory = $false, ParameterSetName = "code")]
+        [switch]$noWrap,
+
+        [Parameter(Position = 0, Mandatory = $True, ParameterSetName = "file")]
+        [String]$macroFile
     )
 
     $app = New-Object -ComObject "$officeProduct.Application"
-    if(-not $officeVersion) { $officeVersion = $app.Version } 
+    if (-not $officeVersion) { $officeVersion = $app.Version } 
     $Key = "HKCU:\Software\Microsoft\Office\$officeVersion\$officeProduct\Security\"
-	if(-not (Test-Path $key)) { New-Item $Key }
+    if (-not (Test-Path $key)) { New-Item $Key }
     Set-ItemProperty -Path $Key -Name 'AccessVBOM' -Value 1
-    if (-not $noWrap) {
+
+    if ($macroFile) {
+        $macroCode = Get-Content $macroFile -Raw
+    }
+    elseif (-not $noWrap) {
         $macroCode = "Sub $sub()`n" + $macroCode + "`nEnd Sub"
-    } 
+    }
+
     if ($officeProduct -eq "Word") {
         $doc = $app.Documents.Add()
     }
