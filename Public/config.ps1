bd9ba0e2e70229168b8f99251f7702a5a072c725
clr2of8 <clr2of8@gmail.com>
2023-05-09 13:27:16 -0600

    Integrate Attire Logger

--- Public/config.ps1
+++ Public/config.ps1
@@ -23,8 +23,8 @@ $artConfig = [PSCustomObject]@{
   # [optional] If you need to use a group managed service account in order to rename the computer, enter it here
   gmsaAccount                = $null
 
-  # [optional] Logging Module
-  LoggingModule = 'Default-ExecutionLogger'
+  # [optional] Logging Module, uses Syslog-ExecutionLogger if left blank and the syslogServer and syslogPort are set, otherwise it uses the Default-ExecutionLogger
+  LoggingModule = ''
 
   # [optional] Syslog configuration, default execution logs will be sent to this server:port
   syslogServer               = '' # set to empty string '' if you don't want to log atomic execution details to a syslog server (don't includle http(s):\\)
