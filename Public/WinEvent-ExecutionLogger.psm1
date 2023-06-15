7cf602a47b7e3d222cb375789894114f6fa3bf07
Hare Sudhan <code@0x6c.dev>
2023-06-15 16:00:02 -0400

    winevent logger added

---
+++ Public/WinEvent-ExecutionLogger.psm1
@@ -0,0 +1,30 @@
+function Start-ExecutionLog($startTime, $logPath, $targetHostname, $targetUser, $commandLine, $isWindows) {
+    if($isWindows && -not [System.Diagnostics.EventLog]::Exists('Atomic Red Team')){
+        New-EventLog -Source "Applications and Services Logs" -LogName "Atomic Red Team"
+    }
+}
+
+function Write-ExecutionLog($startTime, $stopTime, $technique, $testNum, $testName, $testGuid, $testExecutor, $testDescription, $command, $logPath, $targetHostname, $targetUser, $res, $isWindows) {
+    $timeUTC = (Get-Date($startTime).toUniversalTime() -uformat "%Y-%m-%dT%H:%M:%SZ").ToString()
+    $timeLocal = (Get-Date($startTime) -uformat "%Y-%m-%dT%H:%M:%SZ").ToString()
+    $msg = [PSCustomObject][ordered]@{ 
+        "Execution Time (UTC)"   = $timeUTC
+        "Execution Time (Local)" = $timeLocal 
+        "Technique"              = $technique
+        "Test Number"            = $testNum
+        "Test Name"              = $testName
+        "Hostname"               = $targetHostname
+        "Username"               = $targetUser
+        "GUID"                   = $testGuid
+        "Tag"                    = "atomicrunner"
+        "CustomTag"              = $artConfig.CustomTag
+        "ProcessId"              = $res.ProcessId
+        "ExitCode"               = $res.ExitCode
+    } 
+    
+    Write-EventLog  -Source "Applications and Services Logs" -LogName "Atomic Red Team" -EventID 3001 -EntryType Information -Message $msg -Category 1 -RawData 10,20
+}
+
+function Stop-ExecutionLog($startTime, $logPath, $targetHostname, $targetUser, $isWindows) {
+
+}
