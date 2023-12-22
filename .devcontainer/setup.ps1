67f241f8ab74b872079a71d2f0fd284d4b60f5b1
Hare Sudhan <code@0x6c.dev>
2023-12-22 05:57:03 +0000

    modify onCreateCommand

--- .devcontainer/setup.ps1
+++ .devcontainer/setup.ps1
@@ -1,5 +1,5 @@
 New-Item $PROFILE -Force
-Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); 
+Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing -ProgressAction SilentlyContinue); 
 Install-AtomicRedTeam -getAtomics
 Write-Output @"
 Import-Module /workspaces/invoke-atomicredteam/Invoke-AtomicRedTeam.psd1
