1f9b65ae97f7b87d8a409d8d78d58843ae0ed16e
Hare Sudhan <code@0x6c.dev>
2023-12-22 05:43:10 +0000

    adding setup script

--- .devcontainer/setup.ps1
+++ .devcontainer/setup.ps1
@@ -1,9 +1,8 @@
 New-Item $PROFILE -Force
 Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); 
 Install-AtomicRedTeam -getAtomics
-Set-Variable -Name "ARTPath" -Value "$HOME/AtomicRedTeam"
 Write-Output @"
 Import-Module /workspaces/invoke-atomicredteam/Invoke-AtomicRedTeam.psd1
-`$PSDefaultParameterValues`["Invoke-AtomicTest:PathToAtomicsFolder"] = "$ARTPath/atomics";
+`$PSDefaultParameterValues`["Invoke-AtomicTest:PathToAtomicsFolder"] = "$HOME/AtomicRedTeam/atomics";
 `$PSDefaultParameterValues`["Invoke-AtomicTest:ExecutionLogPath"]="1.csv";
 "@ > $PROFILE
