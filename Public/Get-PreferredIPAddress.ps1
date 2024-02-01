a6e3db0e45f553c3a8bad25111fb1681df18961e
dwhite9 <not-supplied>
2024-01-31 20:07:10 -0600

    Added bash -c prefix to macos code block. Without it, getting errors that ifconfig does not exist. This is not reproducable via pwsh cli as it works as expected. The error only occurs when running it via the pwsh function with the runner framework.

--- Public/Get-PreferredIPAddress.ps1
+++ Public/Get-PreferredIPAddress.ps1
@@ -3,7 +3,7 @@ function Get-PreferredIPAddress($isWindows) {
         return (Get-NetIPAddress | Where-Object { $_.PrefixOrigin -ne "WellKnown" }).IPAddress
     }
     elseif ($IsMacOS) {
-        return ifconfig -l | xargs -n1 ipconfig getifaddr
+        return bash -c "ifconfig -l | xargs -n1 ipconfig getifaddr"
     }
     elseif ($IsLinux) {
         return ip -4 -br addr show | sed -n -e 's/^.*UP\s* //p' | cut -d "/" -f 1
