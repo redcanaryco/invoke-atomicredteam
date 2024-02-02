666d2e0f26e58890c2ba26612d28f34df2b9ee41
dwhite9 <not-supplied>
2024-02-02 11:19:50 -0600

    provided full path to macos binaries used for collecting IP to prevent a "command not found error".

--- Public/Get-PreferredIPAddress.ps1
+++ Public/Get-PreferredIPAddress.ps1
@@ -3,7 +3,7 @@ function Get-PreferredIPAddress($isWindows) {
         return (Get-NetIPAddress | Where-Object { $_.PrefixOrigin -ne "WellKnown" }).IPAddress
     }
     elseif ($IsMacOS) {
-        return bash -c "ifconfig -l | xargs -n1 ipconfig getifaddr"
+        return /sbin/ifconfig -l | /usr/bin/xargs -n1 /usr/sbin/ipconfig getifaddr
     }
     elseif ($IsLinux) {
         return ip -4 -br addr show | sed -n -e 's/^.*UP\s* //p' | cut -d "/" -f 1
