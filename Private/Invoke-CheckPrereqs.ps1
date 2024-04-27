f3b473a0969b9e594d46dd86a970c38037d06960
Carrie Roberts <clr2of8@gmail.com>
2024-04-27 12:02:05 -0500

    Revert "Calling Invoke-ExecuteCommand bug fix - Invalid parameter order. "

--- Private/Invoke-CheckPrereqs.ps1
+++ Private/Invoke-CheckPrereqs.ps1
@@ -7,7 +7,7 @@ function Invoke-CheckPrereqs ($test, $isElevated, $executionPlatform, $customInp
         $executor = Get-PrereqExecutor $test
         $final_command = Merge-InputArgs $dep.prereq_command $test $customInputArgs $PathToAtomicsFolder
         if ($executor -ne "powershell") { $final_command = ($final_Command.trim()).Replace("`n", " && ") }
-        $res = Invoke-ExecuteCommand -FinalCommand $final_command -Executor $executor -ExecutionPlatform $executionPlatform -TimeoutSeconds $TimeoutSeconds -Session $session
+        $res = Invoke-ExecuteCommand $final_command $executor $executionPlatform $TimeoutSeconds  $session
         $description = Merge-InputArgs $dep.description $test $customInputArgs $PathToAtomicsFolder
         if ($res.ExitCode -ne 0) {
             $FailureReasons.add($description) | Out-Null
